89a1c67cc097ea1f8412de00a2cc9b10e07cbd74
marciocapendeli <silvamc83@gmail.com>
2024-08-23 21:13:46 -0300

    criação-forms

--- controle/visitantes/views.py
+++ controle/visitantes/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-
+from visitantes.forms import VisitanteForm
 
 
 #registro do visitante
 def registrar_visitante(request):
-    return render(request, 'registrar_visitante.html')
+    form = VisitanteForm()
+    context = {
+        "nome_pagina": "Registrar visitante",
+        "form": form,
+    }
+    return render(request, 'registrar_visitante.html', context)
 
 #adicionado
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html') 
+
