e9b88c55fc3fcaa937d595d7137c70abfb783272
Marcio Silva <silvamc83@gmail.com>
2024-08-27 09:55:38 -0300

    ultimo

--- controle/visitantes/views.py
+++ controle/visitantes/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from visitantes.forms import VisitanteForm
 
-
-#registro do visitante
-def registrar_visitante(request):
-    form = VisitanteForm()
-    context = {
-        "nome_pagina": "Registrar visitante",
-        "form": form,
-    }
-    return render(request, 'registrar_visitante.html', context)
-
 #adicionado
 def index(request):
     return render(request, 'index.html') 
 
+#registro do visitante
+def registrar_visitante(request):
+    form = VisitanteForm()
+
+    if request.method == "POST":
+        form = VisitanteForm(request.POST)
+
+        if form.is_valid():
+            visitante = form.save(commit = False)
+            visitante.registrado_por = request.user.porteiro
+            visitante.save()
+            return redirect("index")
+
+    context = {
+        "nome_pagina": "Registrar visitante",
+        "form": form,
+    }
+
+    return render(request, "registrar_visitante.html", context)
